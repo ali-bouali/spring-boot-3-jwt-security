c488f5fbad974649d9c31e7bf3e97af8fbe13f65
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-01 01:14:54 +0300

    refactor: add missing annotation

--- src/main/java/com/alibou/security/user/UserRepository.java
+++ src/main/java/com/alibou/security/user/UserRepository.java
@@ -1,10 +1,12 @@
 package com.alibou.security.user;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-  Optional<User> findByEmail(String email);
-
+    Optional<User> findByEmail(String email);
 }
