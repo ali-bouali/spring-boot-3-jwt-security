cd431f9808df93f36d8d90ef1ce8469ec67ce338
Murat Yıldız <murat.yildiz@hotmail.co.uk>
2023-04-01 01:13:21 +0300

    refactor: remove 'controller' suffix from endpoint

--- src/main/java/com/alibou/security/demo/DemoController.java
+++ src/main/java/com/alibou/security/demo/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/demo-controller")
+@RequestMapping("/api/v1/demo")
 public class DemoController {
 
   @GetMapping
