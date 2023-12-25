e5e551dc67a835b3d9e40ed8119d32976b79d5f2
Shar <93833319+Sharkend@users.noreply.github.com>
2023-12-25 10:48:49 +0530

    fixed: logical expression in TokenRepository

--- src/main/java/com/alibou/security/token/TokenRepository.java
+++ src/main/java/com/alibou/security/token/TokenRepository.java
@@ -10,7 +10,7 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
   @Query(value = """
       select t from Token t inner join User u\s
       on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      where u.id = :id and (t.expired = false and t.revoked = false)\s
       """)
   List<Token> findAllValidTokenByUser(Integer id);
 
